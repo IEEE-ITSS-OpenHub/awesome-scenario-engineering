e553bc7b06e12e33d7d4549befc149e0bbb88121
Yun Tang <yun.tang@warwick.ac.uk>
2023-09-10 21:36:24 +0100

    fix the github action build with next.config.js update. based URL typo fix

--- common/url.utils.ts
+++ common/url.utils.ts
@@ -2,6 +2,6 @@ export function getBaseURL() {
     if (process && process.env && process.env.NODE_ENV === "development") {
         return "http://localhost:3000";
     } else {
-        return "https://IEEE-ITSS-OpenHub.github.io/awesome-scenario-engineering"
+        return "https://ieee-itss-openhub.github.io/awesome-scenario-engineering"
     }
 }
